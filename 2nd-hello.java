3d365252fb8de91f9ed31c9f01b8c693c8e31947
SHAFI2029 <117617551+SHAFI2029@users.noreply.github.com>
2022-11-10 21:41:12 +0530

    Update 2nd-hello.java

--- 2nd-hello.java
+++ 2nd-hello.java
@@ -1,5 +1,6 @@
 class Solution{
   public static void main(String[] args){
      System.out.println("hello..2nd-file-1st update");
+     System.out.println("hello..2nd-file-2nd update"); 
      }
 }     
